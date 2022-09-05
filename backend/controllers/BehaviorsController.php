6a79d7743eda2838cf2cd59fd543b271ed9d895e
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2022-09-05 21:02:43 +0300

    Completely finished BehaviorsController, RBAC type

--- backend/controllers/BehaviorsController.php
+++ backend/controllers/BehaviorsController.php
@@ -2,7 +2,53 @@
 
 namespace backend\controllers;
 
-class BehaviorsController
-{
+use yii\web\Controller;
+use yii\filters\AccessControl;
 
+/**
+ * Behaviors Controller
+ */
+class BehaviorsController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'controllers' => ['site'],
+                        'actions' => ['index', 'error'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    [
+                        'controllers' => ['site'],
+                        'actions' => ['login', 'registration', 'error'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'allow' => true,
+                        'controllers' => ['site'],
+                        'actions' => ['logout', 'index'],
+                        'verbs' => ['POST'],
+                        'roles' => ['@']
+                    ],
+                    [
+                        'controllers' => ['category'],
+                        'actions' => ['index', 'create', 'update', 'view', 'delete', 'error'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    [
+                        'controllers' => ['post'],
+                        'actions' => ['index', 'create', 'update', 'view', 'delete', 'delete-image', 'error'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ]
+            ],
+        ];
+    }
 }
