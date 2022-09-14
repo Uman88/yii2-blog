fe7bafcf6c0554a1c2ffae10c8981e046ecef5df
Victor Umanskiy <10357172+Uman88@users.noreply.github.com>
2022-09-14 19:14:35 +0300

    Cleaned lines from garbage

--- web/index.php
+++ web/index.php
@@ -6,16 +6,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../common/config/bootstrap.php';
-//require __DIR__ . '/../frontend/config/bootstrap.php';
-
-//$config = yii\helpers\ArrayHelper::merge(
-//    require __DIR__ . '/../common/config/main.php',
-//    require __DIR__ . '/../common/config/main-local.php',
-//    require __DIR__ . '/../frontend/config/main.php',
-//    require __DIR__ . '/../frontend/config/main-local.php'
-//);
-
-require __DIR__ . '/../common/config/bootstrap.php';
 
 if (($pos = strpos($_SERVER['REQUEST_URI'], 'admin')) === 1) {
     require __DIR__ . '/../backend/config/bootstrap.php';
